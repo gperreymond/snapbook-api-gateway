c49beb9427c245f5b28b1b8e9c58c419b94f1599
PERREYMOND Gilles <gperreymond@gmail.com>
2015-09-21 13:28:31 +0000

    commit with script.sh

--- test/microservice-slack.js
+++ test/microservice-slack.js
@@ -1,50 +1,83 @@
 "use strict";
 
-var Code = require('code');
-var Lab = require('lab');
-var lab = exports.lab = Lab.script();
- 
-var describe = lab.describe;
-var it = lab.it;
-var before = lab.before;
-var after = lab.after;
-var expect = Code.expect;
+var chai = require('chai');
+var expect = chai.expect;
+var chaiHttp = require('chai-http');
+chai.use(chaiHttp);
 
-var superagent = require('superagent');
-var assert = require("assert");
-var expect = require('expect.js');
-var should = require('should');
-
-describe('API GATEWAY', function () {
-
-  describe('[get]/swagger', function () {
-    it('should return an error when the gateway is not running', function (done) {
-      superagent
-      .get('http://localhost:3000/swagger')
-      .end(function(err, res) {
-        assert.ifError(err);
+describe('/swagger', function () {
+  it('should return ok when the gateway is running', function (done) {
+    chai.request('http://localhost:3000')
+    .get('/swagger')
+    .end(function(err, res) {
+      try {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
         done();
-      });
+      } catch (e) {
+        done(e);
+      }
     });
+  });
+});
+
+describe('/slack/message', function () {
+  it('should return 404 when the microservice slack is not in post', function (done) {
+    chai.request('http://localhost:3000')
+    .get('/slack/message')
+    .end(function(err, res) {
+      try {
+        expect(res.body.statusCode).to.equal(404);
+        expect(res.body.error).to.have.string('Not Found');
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+  });
+  describe('when microservice not running', function() {
+    it('should return a 404 with an explicit message', function (done) {
+    chai.request('http://localhost:3000')
+    .post('/slack/message')
+    .type('form')
+    .send({
+      state: 'STATE', 
+      source: 'SOURCE',
+      title: 'TITLE',
+      message: 'MESSAGE'
+    })
+    .end(function(err, res) {
+      try {
+        expect(res.body.statusCode).to.equal(404);
+        expect(res.body.message).to.have.string('Service not registered');
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+  });
+  });
+  describe('when microservice is running', function() {
+    it('should return a 200 with data', function (done) {
+    chai.request('http://localhost:3000')
+    .post('/slack/message')
+    .type('form')
+    .send({
+      state: 'STATE', 
+      source: 'SOURCE',
+      title: 'TITLE',
+      message: 'MESSAGE'
+    })
+    .end(function(err, res) {
+      try {
+        expect(res.body.statusCode).to.equal(404);
+        expect(res.body.message).to.have.string('Service not registered');
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+  });
   });
   
-  describe('[post]/slack/message', function () {
-    it('should return an error when the microservice is not running', function (done) {
-      superagent
-      .post('http://localhost:3000/slack/message')
-      .type('form')
-      .send({
-        state: 'STATE', 
-        source: 'SOURCE',
-        title: 'TITLE',
-        message: 'MESSAGE'
-      })
-      .end(function(err, res) {
-        expect(res.body.statusCode).equal(404);
-        expect(res.body.message).to.contain('Service not registered');
-        done();
-      });
-    });
-  });
-
 });

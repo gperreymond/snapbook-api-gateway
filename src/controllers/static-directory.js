bf21501db30f99a84a83454b1938e84526227547
PERREYMOND Gilles <gperreymond@gmail.com>
2015-10-24 21:01:58 +0000

    implements batch on all patterns.

--- src/controllers/static-directory.js
+++ src/controllers/static-directory.js
@@ -1,20 +1,9 @@
 "use strict";
 
-var path = require('path');
-
 exports.media = {
   handler: {
     directory: {
-      path: process.env.SNAPBOOK_API_GATEWAY_VOLUMES_APPLICATIONS || '/home/ubuntu/workspace/applications',
-      listing: true
-    }
-  }
-};
-
-exports.reports = {
-  handler: {
-    directory: {
-      path: path.resolve('reports'),
+      path: '/home/ubuntu/workspace/applications',
       listing: true
     }
   }
